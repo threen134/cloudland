020ba5556d4c2d441db51fc5733ca23e3017218a
Catherine <catherine@raksmart.com>
2026-04-07 10:51:45 +0800

    PET-1361: add gre support in cloudland console
    Signed-off-by: Catherine <catherine@raksmart.com>

--- deploy/roles/hyper/files/firewall.sh
+++ deploy/roles/hyper/files/firewall.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
+iptables -D FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -I FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables -D FORWARD -j REJECT --reject-with icmp-host-prohibited
 iptables -A FORWARD -j REJECT --reject-with icmp-host-prohibited
 
-for chain in $(iptables -S | grep secgroup | awk '{print $2}'); do
-    iptables -X $chain
-done
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
 
 /sbin/iptables-save -c > /etc/iptables.rules
-rm -f /opt/cloudland/run/need_to_sync
